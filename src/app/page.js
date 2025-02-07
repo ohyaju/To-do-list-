465d6dc92c2e342300cb5c9b76753e67a53fc092
ohyaju <bnomin257@gmail.com>
2025-02-07 13:06:33 +0800

    new todo

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,87 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { nanoid } from "nanoid";
+import { useState } from "react";
+
+const initialTasks = [];
+
+export default function Page() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("All");
+
+  function addTask() {
+    const newTaskText = text;
+    if (newTaskText) {
+      const newTasks = [{ text: newTaskText, completed: false, id: nanoid() }, ...tasks];
+      setTasks(newTasks);
+    }
+    setText("");
+  }
+  function handleKeyDown(e) {
+    if (e.code === "Enter") {
+      addTask();
+    }
+  }
+  function toggleCompleted(index) {
+    const clonedTasks = [...tasks];
+    clonedTasks[index].completed = !clonedTasks[index].completed;
+    setTasks(clonedTasks);
+  }
+  function editTask(index) {
+    const updatedtastText = prompt("Task?", tasks[index].text);
+
+    if (updatedtastText) {
+      const clonedTasks = [...tasks];
+      clonedTasks[index].text = updatedtastText;
+      setTasks(clonedTasks);
+    }
+  }
+  function deleteTask(id) {
+    if (confirm("U sure?")) {
+      const clonedTasks = tasks.filter((task) => task.id !== id);
+      setTasks(clonedTasks);
+    }
+  }
+  let filteredTasks = [];
+  if (status === "All") {
+    filteredTasks = tasks;
+  } else {
+    filteredTasks = tasks.filter(filterByStatus);
+  }
+  function filterByStatus(task) {
+    if (status === "Active") {
+      return !task.completed;
+    } else if (status === "Completed") {
+      return task.completed;
+    }
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <input placeholder="New task..." type="text" value={text} onKeyDown={handleKeyDown}
+        onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTask}>Add</button>
+      <div style={{ margin: "12px 0" }}>
+        <button onClick={() => setStatus("All")} style={status === "All" ? { backgroundColor: "black", color: "white" } : {}}>
+          All
+        </button>
+        <button onClick={() => setStatus("Active")} style={status === "Active" ? { backgroundColor: "black", color: "white" } : {}}>
+          Active
+        </button>
+        <button onClick={() => setStatus("Completed")} style={status === "Completed" ? { backgroundColor: "black", color: "white" } : {}}>
+          Completed
+        </button>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      {filteredTasks.map((task, index) => (
+        <div key={task.id} className="task">
+          <input type="checkbox" checked={task.completed} onChange={() => toggleCompleted(index)} />
+          <span className={task.completed ? "done" : ""}>{task.text}</span>
+          <button onClick={() => editTask(index)}>Edit</button>
+          <button onClick={() => deleteTask(task.id)}>Delete</button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
+      {filteredTasks.length}
     </div>
   );
 }
